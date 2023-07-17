5f3dd8b6fb91d611fddc31abdd8cae6e20e5aad0
jeet-ss <jeet.sensarma@gmail.com>
2023-07-17 13:54:02 +0200

    test hpc server

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l #
+#SBATCH --cpus-per-task = 8
+#SBATCH --gres = gpu:gtx3080:1
+#SBATCH --time = 06:00:00 
+#SBATCH --job-name = mpa_mir1k
+#SBATCH --export=NONE 
+
+unset SLURM_EXPORT_ENV 
+
+# cpus-per-task has to be set again for srun
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+module load python/3.8-anaconda
+source source /home/hpc/iwal/iwal131h/mpa/bin/activate
+
+srun python python_setup_check.py
---
+++ python_setup_check.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+import pandas as pd
+import librosa
+import argparse
+import scipy
+import os
+from data_files.dataloader import MedleyDBLoader, MDBMelodySynthLoader, MIR1KLoader
+# extra imports
+from data_files.dataset import CQT_Dataset
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+from utils.model import Spice_model
