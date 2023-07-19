949dd663c51ce3538cc07b63fc3fc5b17986692a
Jeet Sen Sarma <iwal131h@woody3.rrze.uni-erlangen.de>
2023-07-19 12:36:16 +0200

    changed job

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l #
-#SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:rtx3080:1
-#SBATCH --time=01:00:00 
-#SBATCH --job-name=mpa_mir1k
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --time=03:00:00 
+#SBATCH --job-name=mpa_2hr
 #SBATCH --export=NONE 
 
 unset SLURM_EXPORT_ENV 
@@ -10,7 +10,7 @@ unset SLURM_EXPORT_ENV
 # cpus-per-task has to be set again for srun
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
-module load python/3.8-anaconda
-source source /home/hpc/iwal/iwal131h/mpa/bin/activate
+#module load python/3.10-anaconda
+source /home/hpc/iwal/iwal131h/spice/bin/activate
 
-srun python python_setup_check.py
+srun python train.py
