53ebb0047ec85fbfaa1e1968a132703b946375ee
jeet-ss <jeet.sensarma@gmail.com>
2023-08-24 19:51:14 +0200

    added val and encoder out, save and retrain

--- train.py
+++ train.py
@@ -31,12 +31,15 @@ def train(args):
     
     # define Hyperparams
     learning_rate = 1e-4       # original 1e-4
-    epochs_num = 1
+    epochs_st = 0
+    epochs_end = 1000
     batch_size = 64             # original 64
     #tau = 0.1                  # for huber loss
     CQT_bins_per_octave = 24
     wpitch = 3*np.power(10, 4)
     wrecon = 1
+    retrain_epoch = 9
+    name_variant='rev_1k'
 
     ### Architecture params
     # for 1 Unpool
@@ -70,9 +73,10 @@ def train(args):
 
     # Split into batches and Dataloader 
     train, val = train_test_split(data_pd, train_size=0.8, test_size=0.2, random_state=1)
+    print(f'trainData:{train.shape}, valData:{val.shape}')
     train_batches = DataLoader(CQT_Dataset(data=train, mode='train'), batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_batches = DataLoader(CQT_Dataset(data=val, mode='val'), batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    print("no of batches: ", len(train_batches)," sigma: ", sigma_)
+    print(f"train batches:{len(train_batches)},val batches:{len(val_batches)},sigma:{sigma_}")
 
     # set up model 
     spice_u = Spice_model(channel_enc_list, channel_dec_list, unPooling_list)
@@ -92,16 +96,22 @@ def train(args):
     recons_loss = Recons_loss()
     conf_loss = Conf_loss()
     # set up optimizers
-    adam_optim = torch.optim.Adam(spice_u.parameters(), lr=learning_rate)
+    adam_optim = torch.optim.Adam(spice_r.parameters(), lr=learning_rate)
     # set up Trainer object
-    trainer = Trainer(model=spice_u, loss_pitch=pitch_loss, loss_recons=recons_loss, 
+    trainer = Trainer(model=spice_r, loss_pitch=pitch_loss, loss_recons=recons_loss, 
                         loss_conf=conf_loss,
                         optim=adam_optim, train_ds=train_batches, val_test_ds= val_batches,
-                        w_pitch=wpitch, w_recon=wrecon, sigma = sigma_)
+                        w_pitch=wpitch, w_recon=wrecon, sigma = sigma_, name_variant=name_variant)
     # run training
-    loss_train = trainer.fit_model(epochs=epochs_num)
+    trainer.restore_checkpoint(retrain_epoch)
+    loss_data = trainer.fit(epochs_st, epochs_end)
     #trainer.save_model_onnx("check2.onnx")
+    np.save(name_variant+'_data.npy', loss_data)
 
+    # get encoder output
+    #y_hat = trainer.val_test_epoch(batch_data=val_batches,  mode='encoder_out')
+    #np.save(name_variant+'_y_hat.npy', y_hat)
+    #print(yh1.shape)
 
     # plot data
 
